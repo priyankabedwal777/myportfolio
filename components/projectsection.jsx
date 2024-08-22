a47a8f7cf680014c65a85350de2575997744deb5
priyanka-easycampus <priyanka.bedwal@easycampus.ai>
2024-08-22 22:42:10 +0530

    stroke chnages

--- components/projectsection.jsx
+++ components/projectsection.jsx
@@ -119,13 +119,13 @@ const Projectsection = () => {
                                 <Link href={project.gitUrl} className='h-14 w-14 mr-2 border-2 relative rounded-full border-[#ADB7BE] hover:border-white opacity-0 group-hover:opacity-100 transition-opacity duration-500'>
                                     <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor" 
                                         className="h-10 w-10 text-[#ADB7BE] cursor-pointer group-hover:text-white absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2">
-                                        <path strokeLinecap="round" strokeLinejoin="round" d="M17.25 6.75 22.5 12l-5.25 5.25m-10.5 0L1.5 12l5.25-5.25m7.5-3-4.5 16.5" />
+                                        <path d="M17.25 6.75 22.5 12l-5.25 5.25m-10.5 0L1.5 12l5.25-5.25m7.5-3-4.5 16.5" />
                                     </svg>
                                 </Link>
                                 <Link href={project.previewUrl} className='h-14 w-14 border-2 relative rounded-full border-[#ADB7BE] hover:border-white opacity-0 group-hover:opacity-100 transition-opacity duration-500'>
                                 <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor"  className="h-10 w-10 text-[#ADB7BE] cursor-pointer group-hover:text-white absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2">
-                                 <path stroke-linecap="round" stroke-linejoin="round" d="M2.036 12.322a1.012 1.012 0 0 1 0-.639C3.423 7.51 7.36 4.5 12 4.5c4.638 0 8.573 3.007 9.963 7.178.07.207.07.431 0 .639C20.577 16.49 16.64 19.5 12 19.5c-4.638 0-8.573-3.007-9.963-7.178Z" />
-                                 <path stroke-linecap="round" stroke-linejoin="round" d="M15 12a3 3 0 1 1-6 0 3 3 0 0 1 6 0Z" />
+                                 <path  d="M2.036 12.322a1.012 1.012 0 0 1 0-.639C3.423 7.51 7.36 4.5 12 4.5c4.638 0 8.573 3.007 9.963 7.178.07.207.07.431 0 .639C20.577 16.49 16.64 19.5 12 19.5c-4.638 0-8.573-3.007-9.963-7.178Z" />
+                                 <path  d="M15 12a3 3 0 1 1-6 0 3 3 0 0 1 6 0Z" />
                                 </svg>
                                 </Link>
                             </div>
