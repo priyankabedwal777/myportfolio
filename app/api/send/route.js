8e372f25241c9e0786c53d084938a1c0b32b809d
priyanka <114214824+priyankabedwal777@users.noreply.github.com>
2024-11-20 13:20:39 +0530

    commented api routes

--- app/api/send/route.js
+++ app/api/send/route.js
@@ -1,28 +1,28 @@
-// import { EmailTemplate } from '../../../components/EmailTemplate';
-import { Resend } from 'resend';
+// // import { EmailTemplate } from '../../../components/EmailTemplate';
+// import { Resend } from 'resend';
 
-const resend = new Resend(process.env.RESEND_API_KEY);
-const fromEmail = process.env.FROM_EMAIL;
-export async function POST(req, res) {
-  const {body} = await req.json();
-  const {email, subject, message} = body;
-  try {
-    const data = await resend.emails.send({
-      from: fromEmail,
-      to: ["priyankaberwal3@gmail.com", email],
-      subject: 'Hello world',
-      react:(
-        <>
-        <h1>{subject}</h1>
-        <p>Thank You for contacting us!</p>
-        <p>New message submitted</p>
-        <p>{message}</p>
-        </>
-      ),
-    });
+// const resend = new Resend(process.env.RESEND_API_KEY);
+// const fromEmail = process.env.FROM_EMAIL;
+// export async function POST(req, res) {
+//   const {body} = await req.json();
+//   const {email, subject, message} = body;
+//   try {
+//     const data = await resend.emails.send({
+//       from: fromEmail,
+//       to: ["priyankaberwal3@gmail.com", email],
+//       subject: 'Hello world',
+//       react:(
+//         <>
+//         <h1>{subject}</h1>
+//         <p>Thank You for contacting us!</p>
+//         <p>New message submitted</p>
+//         <p>{message}</p>
+//         </>
+//       ),
+//     });
 
-    return Response.json(data);
-  } catch (error) {
-    return Response.json({ error });
-  }
-}
+//     return Response.json(data);
+//   } catch (error) {
+//     return Response.json({ error });
+//   }
+// }
